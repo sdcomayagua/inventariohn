cd30805bbd22ea3657a9af6cdb3f1176cc5f761e
sdcomayagua <sdcomayagua@gmail.com>
2026-05-07 01:07:13 -0600

    1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.SDC_CONFIG = {
   storageKey: 'sdc_panel_privado_estado',
   cloudKey: 'sdc_panel_privado_nube',
-  defaultAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbzJB4A9WhU96M_luuUY_xYEDAnNCuU6dkHRCluszxbjaiPQyiDnF4VZqM6MhghiKsV0/exec',
+  defaultAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbyrdvBs3UQ5Jwo-l8ykfMf8qfPKpGqdf6aBU6pW8wZvl1XFmJmf7CUSTI7BGz7etG5i/exec',
   whatsapp: '50431517755',
   storeName: 'SD COMAYAGUA',
   versionName: 'Nube actualizada'

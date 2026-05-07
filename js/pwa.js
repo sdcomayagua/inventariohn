33671032e5319bdc927aee7f8e8627869963ae47
sdcomayagua <sdcomayagua@gmail.com>
2026-05-07 00:51:49 -0600

    d

--- js/pwa.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  if (!('serviceWorker' in navigator)) return;
-  window.addEventListener('load', function(){
-    navigator.serviceWorker.register('./sw.js').catch(function(err){
-      console.warn('SDC PWA no pudo registrarse:', err);
-    });
-  });
-})();

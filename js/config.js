c64a7859ca43aa146c2dccc6d6e8b674c01432b8
sdcomayagua <sdcomayagua@gmail.com>
2026-05-07 00:57:46 -0600

    1

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window.SDC_CONFIG = {
+  storageKey: 'sdc_panel_privado_estado',
+  cloudKey: 'sdc_panel_privado_nube',
+  defaultAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbzJB4A9WhU96M_luuUY_xYEDAnNCuU6dkHRCluszxbjaiPQyiDnF4VZqM6MhghiKsV0/exec',
+  whatsapp: '50431517755',
+  storeName: 'SD COMAYAGUA',
+  versionName: 'Nube actualizada'
+};

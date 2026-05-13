ad8828402c39b5ae3850331e01a58c5559c1919a
sdcomayagua <sdcomayagua@gmail.com>
2026-05-12 22:37:05 -0600

    d

--- js/sdc-v53-mobile-clean.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  function boot(){
-    if(!document.body) return;
-    document.body.classList.add('sdc-v53-mobile-clean');
-  }
-  if(document.readyState==='loading') document.addEventListener('DOMContentLoaded',boot,{once:true});
-  else boot();
-})();

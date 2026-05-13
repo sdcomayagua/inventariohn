b92e864c3152ddb4a889698e2ad41baad41305ab
sdcomayagua <sdcomayagua@gmail.com>
2026-05-13 09:54:31 -0600

    D

--- js/app.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.sheets-btn').addEventListener('click', function() {
-    alert("Actualizando datos en Google Sheets...");
-    // Aquí puedes integrar la lógica para actualizar datos en Google Sheets
-    // Por ejemplo, mediante una API o un servicio que conecte con Google Sheets
-});

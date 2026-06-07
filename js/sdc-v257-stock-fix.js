b7afeca15bd1eb2f488edb255130936740849e6b
sdcomayagua <sdcomayagua@gmail.com>
2026-06-07 12:32:18 -0600

    Cargar mejoras v297 de copiado producto

--- js/sdc-v257-stock-fix.js
+++ js/sdc-v257-stock-fix.js
@@ -1,1 +1,9 @@
 document.documentElement.dataset.sdcV257='1';
+(function(){
+  if(document.getElementById('sdc-v297-product-copy-loader')) return;
+  var s=document.createElement('script');
+  s.id='sdc-v297-product-copy-loader';
+  s.defer=true;
+  s.src='js/sdc-v297-product-copy.js?v=297-product-copy-1';
+  document.head.appendChild(s);
+})();

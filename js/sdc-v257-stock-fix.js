8e0f7a5d1dffe912f49f2c22cc34e49c42a8ef51
sdcomayagua <sdcomayagua@gmail.com>
2026-06-07 12:44:35 -0600

    Cargar parche visual v298

--- js/sdc-v257-stock-fix.js
+++ js/sdc-v257-stock-fix.js
@@ -1,9 +1,17 @@
 document.documentElement.dataset.sdcV257='1';
 (function(){
-  if(document.getElementById('sdc-v297-product-copy-loader')) return;
-  var s=document.createElement('script');
-  s.id='sdc-v297-product-copy-loader';
-  s.defer=true;
-  s.src='js/sdc-v297-product-copy.js?v=297-product-copy-1';
-  document.head.appendChild(s);
+  if(!document.getElementById('sdc-v297-product-copy-loader')){
+    var s=document.createElement('script');
+    s.id='sdc-v297-product-copy-loader';
+    s.defer=true;
+    s.src='js/sdc-v297-product-copy.js?v=297-product-copy-1';
+    document.head.appendChild(s);
+  }
+  if(!document.getElementById('sdc-v298-fixes-loader')){
+    var f=document.createElement('script');
+    f.id='sdc-v298-fixes-loader';
+    f.defer=true;
+    f.src='js/sdc-v298-fixes-salman.js?v=298-fixes-1';
+    document.head.appendChild(f);
+  }
 })();

f9cc70a45bb3ae16a1ab9ab4ffde5763be87c60d
sdcomayagua <sdcomayagua@gmail.com>
2026-06-07 13:11:17 -0600

    Cargar ficha IA WhatsApp v300

--- js/sdc-v257-stock-fix.js
+++ js/sdc-v257-stock-fix.js
@@ -8,4 +8,24 @@ document.documentElement.dataset.sdcV257='1';
     document.head.appendChild(s);
   }
   if(!document.getElementById('sdc-v298-fixes-loader')){
-    var f=document.createElement('script
+    var f=document.createElement('script');
+    f.id='sdc-v298-fixes-loader';
+    f.defer=true;
+    f.src='js/sdc-v298-fixes-salman.js?v=298-fixes-1';
+    document.head.appendChild(f);
+  }
+  if(!document.getElementById('sdc-v299-final-loader')){
+    var p=document.createElement('script');
+    p.id='sdc-v299-final-loader';
+    p.defer=true;
+    p.src='js/sdc-v299-final-polish.js?v=299-final-1';
+    document.head.appendChild(p);
+  }
+  if(!document.getElementById('sdc-v300-ia-loader')){
+    var ia=document.createElement('script');
+    ia.id='sdc-v300-ia-loader';
+    ia.defer=true;
+    ia.src='js/sdc-v300-ia-whatsapp.js?v=300-ia-1';
+    document.head.appendChild(ia);
+  }
+})();

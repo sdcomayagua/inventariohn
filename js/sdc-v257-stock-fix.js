ae837112ad9f8e43310b6af371e69ad03966af88
sdcomayagua <sdcomayagua@gmail.com>
2026-06-08 00:31:31 -0600

    Cargar ajustes visuales finales

--- js/sdc-v257-stock-fix.js
+++ js/sdc-v257-stock-fix.js
@@ -1,11 +1,18 @@
-document.documentElement.dataset.sdcV257='stable-share-v302-b';
+document.documentElement.dataset.sdcV257='stable-share-v303-polish';
 (function(){
-  console.info('SDC: modo estable activo con botones livianos v302-b.');
+  console.info('SDC: modo estable activo con botones livianos v303 + polish.');
+  if(!document.getElementById('sdc-v303-user-polish-final')){
+    var c=document.createElement('link');
+    c.id='sdc-v303-user-polish-final';
+    c.rel='stylesheet';
+    c.href='css/sdc-v303-user-polish-final.css?v=303-final-1';
+    document.head.appendChild(c);
+  }
   if(!document.getElementById('sdc-v302-share-safe-loader')){
     var s=document.createElement('script');
     s.id='sdc-v302-share-safe-loader';
     s.defer=true;
-    s.src='js/sdc-v302-share-safe.js?v=302-safe-2';
+    s.src='js/sdc-v302-share-safe.js?v=302-safe-3';
     document.head.appendChild(s);
   }
 })();

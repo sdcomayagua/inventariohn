ebee806257085a46f8f684ba28a64f7b541a82a4
sdcomayagua <sdcomayagua@gmail.com>
2026-06-07 21:12:23 -0600

    Forzar recarga del modulo compartir

--- js/sdc-v257-stock-fix.js
+++ js/sdc-v257-stock-fix.js
@@ -1,11 +1,11 @@
-document.documentElement.dataset.sdcV257='stable-share-v302';
+document.documentElement.dataset.sdcV257='stable-share-v302-b';
 (function(){
-  console.info('SDC: modo estable activo con botones livianos v302.');
+  console.info('SDC: modo estable activo con botones livianos v302-b.');
   if(!document.getElementById('sdc-v302-share-safe-loader')){
     var s=document.createElement('script');
     s.id='sdc-v302-share-safe-loader';
     s.defer=true;
-    s.src='js/sdc-v302-share-safe.js?v=302-safe-1';
+    s.src='js/sdc-v302-share-safe.js?v=302-safe-2';
     document.head.appendChild(s);
   }
 })();
